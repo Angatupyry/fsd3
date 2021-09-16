be5ee2f066b1874b89db4e8b44147a2021161f54
Angatupyry <fierrofenix@gmail.com>
2021-09-16 08:49:23 -0400

    Se agregan cambios en nodeJS

---
+++ Node/Clase2/ejercicio2.js
@@ -0,0 +1,7 @@
+console.log("Comienza el programa...");
+
+setTimeout(() => {
+  console.log("Invocación del setTimeout");
+}, 0);
+
+console.log("Termina el programa");
---
+++ Node/Clase2/ejercicios1.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+
+// let data = fs.readFileSync("text.txt");
+
+// console.log(data.toString());
+// console.log("Programa terminado");
+
+fs.readFile("text.txt", function name(error, data) {
+  if (error) return console.log(error);
+  console.log(data.toString());
+});
+
+console.log("Programa terminado");
